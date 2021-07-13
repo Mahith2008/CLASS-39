c94c9cfa8972958beeb0217046a9c2c872c243d5
unknown <saralaswarna.s@gmail.com>
2021-07-13 14:42:32 +0530

    sprites and images added

--- sketch.js
+++ sketch.js
@@ -5,14 +5,27 @@ var playerCount;
 var allPlayers;
 var distance = 0;
 var database;
-
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
 
+var car1Img,car2Img,car3Img,car4Img;
+var groungImg;
+var trackImg;
+
+
+function preload(){
+
+  car1Img = loadImage("images/car1.png");
+  car2Img = loadImage("images/car2.png");
+  car3Img = loadImage("images/car3.png");
+  car4Img = loadImage("images/car4.png");
+  groundImg = loadImage("images/ground.png");
+  trackImg = loadImage("images/track.jpg");
+}
 
 function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  canvas = createCanvas(displayWidth, displayHeight);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -27,5 +40,10 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+
+  if(gameState === 2){
+
+    game.end();
   }
 }
